a8d32d0977bfb1609efd83418d154667142af49c
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-03-02 15:25:59 +0100

    finishing backend logic

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const tasks = require('./routes/tasks');
 
 app.use(express.static('./public'));
+app.use(express.json());
+
 app.use('/api/v1/tasks', tasks);
 
-const PORT = 3000;
+app.use(errorHandlerMiddleware);
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}...`);
-});
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, async () => {
+      console.log(`Server is listening on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
